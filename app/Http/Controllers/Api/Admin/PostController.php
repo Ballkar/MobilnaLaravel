cb9c46dceb8ffb6e3c123863ca9c9498335c8167
Arkadiusz <arek.l@elementzone.uk>
2020-01-16 18:01:08 +0100

    add latest in post controller

--- app/Http/Controllers/Api/Admin/PostController.php
+++ app/Http/Controllers/Api/Admin/PostController.php
@@ -19,7 +19,7 @@ class PostController extends Controller
 
     public function index()
     {
-        $posts = Post::paginate(10);
+        $posts = Post::latest()->paginate(10);
         return $this->sendResponse($posts, 'Posts returned');
     }
 
