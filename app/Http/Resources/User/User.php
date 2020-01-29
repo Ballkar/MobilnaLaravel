7c80927ed4d01a6fbf0732699bd91ab95426db7d
Arkadiusz <arek.l@elementzone.uk>
2020-01-29 23:40:23 +0100

    userResource

--- app/Http/Resources/User/User.php
+++ app/Http/Resources/User/User.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\User;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class User extends JsonResource
 {
@@ -15,23 +16,31 @@ class User extends JsonResource
      */
     public function toArray($request)
     {
+        $authUser = $request->user('api');
         return [
             'id' => $this->id,
             'email' => $this->email,
-            'role_id' => $this->role_id,
-            'reg' => $this->reg,
-
             'name' => $this->name,
             'surname' => $this->surname,
-            'phone' => substr($this->phone, 0, 3).preg_replace("/[0-9]/", '*', substr($this->phone, 3)),
             'city' => $this->city,
             'road' => $this->road,
             'house_number' => $this->house_number,
             'flat_number' => $this->flat_number,
-            'birth_date' => $this->birth_date,
             'additional_info' => $this->additional_info,
             'avatar' => env('APP_URL').'/storage/users/'.$this->id.'/avatar/'.$this->avatar,
             'avatar_thumbnail' => env('APP_URL').'/storage/users/'.$this->id.'/avatar/'.$this->avatar_thumbnail,
+
+            $this->mergeWhen((!$authUser || !$authUser->isAdmin()) && $authUser->id !== $this->id, [
+                'phone' => substr($this->phone, 0, 3).preg_replace("/[0-9]/", '*', substr($this->phone, 3)),
+            ]),
+
+            $this->mergeWhen($authUser && ($authUser->isAdmin() || $authUser->id === $this->id), [
+                'phone' => $this->phone,
+                'reg' => $this->reg,
+                'role_id' => $this->role_id,
+                'birth_date' => $this->birth_date,
+            ]),
+
         ];
     }
 }
