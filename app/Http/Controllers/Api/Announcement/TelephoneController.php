ef60b11ee9656e42cba9b148d3762d8d843cbb76
Arkadiusz <arek.l@elementzone.uk>
2020-02-13 18:50:47 +0100

    hide telephone in announcements

---
+++ app/Http/Controllers/Api/Announcement/TelephoneController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\Announcement;
+
+use App\Models\Announcement\Announcement;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TelephoneController extends Controller
+{
+    public function show(Announcement $announcement)
+    {
+
+    }
+}
--- app/Http/Resources/Announcement/Announcement.php
+++ app/Http/Resources/Announcement/Announcement.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources\Announcement;
 use App\Http\Resources\Announcement\Service\Service as ServiceResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class Announcement extends JsonResource
 {
@@ -36,6 +37,12 @@ class Announcement extends JsonResource
                 'road' => $this->road,
                 'house_number' => $this->house_number,
                 'flat_number' => $this->flat_number,
+            ]),
+            $this->mergeWhen(Auth::guard('api')->user()->isAdmin() || Auth::guard('api')->check() && $this->owner->id == Auth::guard('api')->id(), [
+                'phone' => $this->owner->phone,
+            ]),
+            $this->mergeWhen($this->owner->id !== Auth::guard('api')->id(), [
+                'phone' => substr($this->owner->phone, 0, 3).preg_replace("/[0-9]/", '*', substr($this->owner->phone, 3)),
             ]),
         ];
     }
