08c633da61371c9e7cd7a1a6be885b66bb2fe338
Arkadiusz <arkadiusz.latka@sembot.io>
2020-10-30 14:27:34 +0100

    repair customer resource

--- app/Http/Resources/Announcement/Customer.php
+++ app/Http/Resources/Announcement/Customer.php
@@ -17,22 +17,10 @@ class Customer extends JsonResource
     {
         return [
             'id' => $this->id,
-            'user_id' => $this->user_id,
             'name' => $this->name,
             'surname' => $this->surname,
             'additional_info' => $this->additional_info,
-
-            $this->mergeWhen($this->owner_id === $request->user('api')->id, [
-                'phone' => $this->phone,
-                'birth_date' => $this->birth_date,
-                'city_id' => $this->city_id,
-                'road' => $this->road,
-                'house_number' => $this->house_number,
-                'flat_number' => $this->flat_number,
-            ]),
-
-            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
-
+            'phone' => $this->phone,
         ];
     }
 }
