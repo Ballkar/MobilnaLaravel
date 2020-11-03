b415f5d80056a5fd92c08cbb89d7c19e35e084fd
arkadiusz-latka <arkadiusz.latka@sembot.io>
2020-11-03 22:21:49 +0100

    base returning works from api

---
+++ app/Http/Resources/Calendar/Work.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\Calendar;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Work extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $customer = $this->customer;
+        return [
+            'id' => $this->id,
+            'start' => $this->start,
+            'stop' => $this->stop,
+            'customer' => [
+                'id' => $customer->id,
+                'phone' => $customer->phone,
+                'name' => $customer->name,
+                'surname' => $customer->surname,
+            ],
+        ];
+    }
+}
